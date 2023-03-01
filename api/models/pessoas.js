f02aad7239897975041a0da87a4f7bd056402aed
Douglas Leal <douglas.leal@live.com>
2023-03-01 12:00:49 -0300

    Associando os modelos

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -10,7 +10,12 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.hasMany(models.Turmas, {
+        foreignKey: 'docente_id'
+      });
+      this.hasMany(models.Matriculas, {
+        foreignKey: 'estudante_id'
+      });
     }
   }
   Pessoas.init({
--- api/models/turmas.js
+++ api/models/turmas.js
@@ -10,7 +10,11 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.hasMany(models.Matriculas, {
+        foreignKey: 'turma_id'
+      });
+      this.belongsTo(models.Pessoas);
+      this.belongsTo(models.Niveis)
     }
   }
   Turmas.init({
